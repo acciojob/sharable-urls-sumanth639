aff4f57354d5dca5eaa403354e3918b23499383c
Sumanth Poojary <128285327+sumanth639@users.noreply.github.com>
2025-11-23 19:48:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 // your code here
+
+let button=document.getElementById('button')
+let url=document.getElementById('url')
+
+
+
+
+
+function getURL(){
+
+	let name=document.getElementById('name').value.trim()
+    let year=document.getElementById('year').value.trim()
+
+	let baseURL = "https://localhost:8080/";
+
+	if(name||year){
+		baseURL+='?'
+
+		if(name) baseURL+=`name=${name}`
+		if(name && year) baseURL+=`&year=${year}`
+		if(!name && year) baseURL+=`year=${year}`
+	}
+
+	url.innerText=baseURL;
+	
+  
+}
+
+
+button.addEventListener('click',getURL)
+
+
+
+
+
+
+
+
+
